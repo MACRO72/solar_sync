4a26aa6cbffba2125cbb48d2f3fd9f33430a251b
Abinash Jena <abinashjena2401@gmail.com>
2025-10-29 19:35:46 +0000

    give boarder radius to the cards

--- src/components/glass-card.tsx
+++ src/components/glass-card.tsx
@@ -10,7 +10,7 @@ const GlassCard = React.forwardRef<
   <Card
     ref={ref}
     className={cn(
-      "bg-card/60 backdrop-blur-sm border border-border/20 shadow-lg bg-cover bg-center bg-blend-screen",
+      "rounded-lg bg-card/60 backdrop-blur-sm border border-border/20 shadow-lg bg-cover bg-center bg-blend-screen",
       "bg-[url('https://images.unsplash.com/photo-1557682250-33bd709cbe85?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]",
       className
     )}
