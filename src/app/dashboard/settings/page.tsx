279e13e2bf7977d07ed02026d039e453e982ee82
Nihar Ranjan Behera <niharlikunbehera@gmail.com>
2025-10-30 17:32:41 +0000

    delete the profile settings from the settings page only

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,5 +1,4 @@
 
-import { UserProfileForm } from './components/user-profile-form';
 import { NotificationSettings } from './components/notification-settings';
 import { AppInformation } from './components/app-information';
 import { HelpAndSupport } from './components/help-support';
@@ -8,7 +7,6 @@ export default function SettingsPage() {
   return (
     <div className="grid gap-6 lg:grid-cols-2">
         <div className="space-y-6">
-            <UserProfileForm />
             <AppInformation />
         </div>
         <div className="space-y-6">
